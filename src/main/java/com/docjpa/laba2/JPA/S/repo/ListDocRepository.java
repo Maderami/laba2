dfbe22d6cdf51dc552e71c0d8d9c92a4248c756f
Madermi <sdsoerty6584@gmail.com>
2021-11-18 00:08:50 +0600

    sad

---
+++ src/main/java/com/docjpa/laba2/JPA/S/repo/ListDocRepository.java
@@ -0,0 +1,9 @@
+package com.docjpa.laba2.JPA.S.repo;
+
+import com.docjpa.laba2.JPA.S.models.ListDocs;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ListDocRepository extends CrudRepository<ListDocs, Long> {
+}
