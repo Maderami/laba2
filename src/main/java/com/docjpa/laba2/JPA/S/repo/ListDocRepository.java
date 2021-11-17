9e07699f370b1405052d2b505a54ce6c5da3a832
Maderami <32415939+Maderami@users.noreply.github.com>
2021-11-17 23:52:09 +0600

    Delete src directory

--- src/main/java/com/docjpa/laba2/JPA/S/repo/ListDocRepository.java
+++
@@ -1,9 +0,0 @@
-package com.docjpa.laba2.JPA.S.repo;
-
-import com.docjpa.laba2.JPA.S.models.ListDocs;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface ListDocRepository extends CrudRepository<ListDocs, Long> {
-}
